3628a0d86de7960509e182142711ffbd59ae6d78
mathigithubprakash <122209273+mathigithubprakash@users.noreply.github.com>
2024-01-04 09:42:27 +0530

    Add files via upload

---
+++ case.rb
@@ -0,0 +1,10 @@
+print "enter the city : "
+choice = gets.chomp
+case choice 
+    when "tvl"
+          puts "tirunelveli"
+    when "mdu"
+          puts "madurai"
+    else 
+          puts "unknown"
+  end
